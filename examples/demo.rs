0c5925e7ed0960804663c4f28a866cc7e5c08e1e
anasxyz <anaswessam176@gmail.com>
2026-02-23 16:54:02 +0000

    m

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,43 +1,58 @@
-use bento::*;
+#![allow(dead_code, unused)]
 
-struct Counter {
-    count: i32,
-}
+use bento::*;
 
 #[derive(Clone)]
 enum Action {
-    Increment,
-    Decrement,
+    UpdateAddress(String),
 }
 
-impl App for Counter {
+struct MyApp {
+    count: i32,
+    address: String,
+}
+
+impl App for MyApp {
     type Action = Action;
 
     fn new() -> Self {
-        Self { count: 0 }
-    }
-
-    fn update(&mut self, action: Action) -> Vec<Task<Action>> {
-        match action {
-            Action::Increment => self.count += 1,
-            Action::Decrement => self.count -= 1,
+        Self { 
+            count: 0,
+            address: String::new()
         }
-        vec![]
     }
 
     fn view(&self) -> Element<Action> {
         column(vec![
-            text(&self.count.to_string(), Color::WHITE).font_size(48.0),
-            button("−").on_click(Action::Decrement),
-            button("+").on_click(Action::Increment),
+            text("Billing address:", Color::hex("#1f282d"))
+                .font_size(12.0)
+                .font_weight(600),
+            text_input("address")
+                .value(&self.address)
+                .on_change(|v| Action::UpdateAddress(v))
+                .placeholder("Enter address")
+                .font_size(12.0)
+                .border(Color::hex("#000000"), 1.0)
+                .border_radius(2.0)
+                .margin(Margin::top(3.0))
+                .background(Color::hex("#ffffff"))
+                .text_color(Color::hex("#1f282d"))
+                .width(percent(100.0)),
         ])
-        .gap(16.0)
         .background(Color::hex("#eeeeee"))
+        .padding(Edges::all(5.0))
         .width(percent(100.0))
         .height(percent(100.0))
+    }
+
+    fn update(&mut self, action: Action) -> Vec<Task<Action>> {
+        match action {
+            Action::UpdateAddress(v) => self.address = v,
+        }
+        vec![]
     }
 }
 
 fn main() {
-    Counter::run(Settings::default().title("demo"));
+    MyApp::run(Settings::default().clear_color(Color::hex("#eeeeee")));
 }
